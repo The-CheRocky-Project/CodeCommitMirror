319ab78aebc9a358cd5789b20c6ca6c173dc1173
Mirco <giardinamirco@gmail.com>
2020-04-29 10:45:24 +0200

    Divisa fileExplorerTemplate.hbs e tileTemplate.hbs
    Corretta inoltre la funzione createTile() ora che il metodo di visualizzazione è cambiato

--- nodeJS/src/views/fileExplorerView.js
+++ nodeJS/src/views/fileExplorerView.js
@@ -2,6 +2,10 @@
 
 //require the template
 //const body= require('../templates/fileExplorerTemplate.hbs');
+const hbs = require('hbs');
+const fs = require('fs');
+const path = require('path');
+
 /**
  * Effettua il rendering del body della pagina di visualizzazione dei file in base ai parametri contenuti nel json fileList. Se fileList
  * contiene una lista vuota farà il rendereing di un avvertimento.
@@ -11,7 +15,7 @@
  */
 exports.print = (req,res,fileList) => {
     //Scorro tutti i file nel json e costruisco le singole tiles
-    let tiles = fileList.files.map(function(singleFile){
+    let tiles = fileList.files.map((singleFile) => {
         return createTile(singleFile.fileKey,singleFile.thumbnailUrl);
     });
     //Se la dimensione della variabile tiles è 0 vuol dire che non ci sono file da visualizzare
@@ -26,22 +30,8 @@ exports.print = (req,res,fileList) => {
  * @returns {string} tile - Html della tile popolata con i parametri dati
  */
 function createTile(fileKey, thumbnailUrl) {
-    return [
-        '<div class="col-md-4">',
-            '<div class="card mb-4 shadow-sm">',
-                '<img src="' + thumbnailUrl + '" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail" title="Descrizione immagine"></img>',
-                //Versione con placeholder per la thumbnail invece che usare l'url
-                //'<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>',
-                '<div class="card-body">',
-                    '<p>' + fileKey + '</p>',
-                    '<div class="d-flex justify-content-between align-items-center">',
-                        '<div class="btn-group">',
-                            '<button type="button" class="btn btn-sm btn-outline-secondary">Scarica</button>',
-                            '<button type="button" class="btn btn-sm btn-outline-secondary">Elabora</button>',
-                        '</div>',
-                    '</div>',
-                '</div>',
-            '</div>',
-        '</div>'
-    ].join('\n');
+    //Carico il template della singola tile e lo compilo con i dati
+    const tileTemplate = fs.readFileSync(path.join(__dirname, 'tileTemplate.hbs'), 'utf8');
+    const template = hbs.compile(tileTemplate);
+    return template({fileKey: fileKey, thumbnailUrl: thumbnailUrl});
 }
