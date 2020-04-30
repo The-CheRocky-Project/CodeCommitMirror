8366135337321b5d4d17d4b02996ec76de15c426
Mattia Gottardello <mattia.gottardello.96@gmail.com>
2020-04-30 16:01:20 +0200

    Added launchFileProcessing()

--- nodeJS/src/controllers/fileExplorerController.js
+++ nodeJS/src/controllers/fileExplorerController.js
@@ -1,16 +1,16 @@
 // fileExplorerController module
 // requires model and view
-let view = require('../views/fileExplorerView');
-let model = require('../models/fileExplorerModel');
+const view = require('../views/fileExplorerView');
+const model = require('../models/fileExplorerModel');
 
 /**
  * Effettua il rendering del body della pagina di visualizzazione dei file in base ai parametri contenuti nel json fileList. Se fileList
  * contiene una lista vuota farà il rendereing di un avvertimento.
- * @param {object} res - Rappresenta la risposta http
+ * @param {object} res - Rappresenta la risposta XHTML
  */
 exports.getBody = (res) => {
     //Variabile fileList di prova (da costruire prendendo i file da s3)
-    const fileKeys = fileExplorerModel.getUpdateFileList();
+    const fileKeys = model.getUpdatedFileList();
     let fileList = Array();
     for (key in fileKeys){
         fileList.push({
@@ -19,4 +19,12 @@ exports.getBody = (res) => {
         });
     }
     view.print(fileList,res);
+}
+
+/**
+ * Effettua la chiamata al model per il lancio del processo di elaborazione
+ * @param {object} fileKey - Rappresenta la risposta XHTML
+ */
+exports.launchFileProcessing = (fileKey) => {
+    model.processFile(fileKey);
 }
