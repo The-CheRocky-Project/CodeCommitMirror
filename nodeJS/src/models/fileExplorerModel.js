a711adf85fa4866975a81bafabdadf06f756e152
Alessio Bettarello <bettaz.developer@gmail.com>
2020-04-30 11:32:08 +0200

    added constructor for fileExplorerModel

--- nodeJS/src/models/fileExplorerModel.js
+++ nodeJS/src/models/fileExplorerModel.js
@@ -1,1 +1,6 @@
-// fileExplorerModel module
+/**
+ * File Explorer Model module.
+ * @module models/fileExplorerModel
+ */
+const s3 = require('../wrappers/s3Wrapper')
+const sns = require('../wrappers/snsWrapper')
