4b199cb58ab07e6e092ca2228ab465d30f74c90e
Alessio Bettarello <bettaz.developer@gmail.com>
2020-04-30 15:17:57 +0200

    initialize the loadingController

--- nodeJS/src/controllers/loadingController.js
+++ nodeJS/src/controllers/loadingController.js
@@ -1,7 +1,11 @@
-// loadingController module
+/**
+ * Loading Controller module.
+ * @module controllers/loadingController
+ */
 // requires model and view
 let view=require('../views/loadingView');
-//model= require('../models/loadingModel');
+model= require('../models/loadingModel');
+
 exports.getBody = (req,res) => {
     view.print(req,res);
 }
