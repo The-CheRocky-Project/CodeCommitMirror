265283eb37b8a2856da00629bc9e81fa3fd56532
Mattia Gottardello <mattia.gottardello.96@gmail.com>
2020-04-30 16:24:45 +0200

    correct getBody(), getUpdatedFileList(), template and view

--- nodeJS/src/views/fileExplorerView.js
+++ nodeJS/src/views/fileExplorerView.js
@@ -7,12 +7,14 @@
 /**
  * Effettua il rendering del body della pagina di visualizzazione dei file in base ai parametri contenuti nel json fileList. Se fileList
  * contiene una lista vuota farà il rendereing di un avvertimento.
- * @param {object} req - Rappresenta la richiesta http
+ * @param {object} param - Rappresenta la richiesta http
  * @param {object} res - Rappresenta la risposta http
  * @param {json} fileList - Lista dei file in formato json contente la key del file e l'url alla sua thumbail
  */
-exports.print = (req,res,fileList) => {
+exports.print = (param,res,) => {
     res.render('layouts/fileExplorerTemplate', {
         template: 'fileExplorerTemplate',
-        data: fileList
-    })};
+        data: param['files'],
+        layout: param['main']
+    });
+};
