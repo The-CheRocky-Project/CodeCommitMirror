560c69d56e4d4cb47722d04e018569756e890d5d
Mattia Gottardello <mattia.gottardello.96@gmail.com>
2020-04-30 17:16:27 +0200

    added listFileKeys, getThumbnailURL

--- nodeJS/src/models/fileExplorerModel.js
+++ nodeJS/src/models/fileExplorerModel.js
@@ -4,3 +4,24 @@
  */
 const s3 = require('../wrappers/s3Wrapper')
 const sns = require('../wrappers/snsWrapper')
+
+/**
+ * return files from the fileExplorer bucket
+ */
+exports.listFilekeys = () =>{
+    return s3.listObject();
+}
+
+/**
+ * return thumbnail URL from a bucket
+ */
+exports.getThumbnailURL = (filekey) =>{
+    return s3.getObjectUrl(filekey);
+}
+
+/**
+ * send a message by snsWrap to serverless application to launch the file elaboration
+ */
+// exports.processFile = (fileKey) =>{
+//     return sns.message(fileKey);
+// }
