9461150847169563dfe0555b714ad8eec8e8f265
Alessio Bettarello <bettaz.developer@gmail.com>
2020-04-30 15:36:02 +0200

    add heading and add loadingView - generateProgressBar(progression)

--- nodeJS/src/views/loadingView.js
+++ nodeJS/src/views/loadingView.js
@@ -1,7 +1,8 @@
-// loadingView module
-//const handlebars = require('handlebars');
-//Register progressBar as a partial
-//handlebars.registerPartial('progressBar','progressBar');
+/**
+ * Loading View module.
+ * @module views/loadingController
+ */
+
 /**
  * Effettua il rendering del body della pagina di caricamento allo stato iniziale
  * @param {object} res - Rappresenta la risposta http
@@ -13,5 +14,17 @@ exports.print = (res) => {
             data:{
                 progression: 0
             }
+        });
+};
+
+/**
+ * Effettua il rendering della progress bar nella percentuale indicata dalla request
+ * @param {object} res - Rappresenta la risposta http
+ */
+exports.generateProgressBar = (res,progress) => {
+    res.render('partials/progressBar',
+        {
+            progression: progress,
+            layout: false
         });
 };
