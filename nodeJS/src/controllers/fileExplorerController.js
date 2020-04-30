387a050502e961258c5234bc0bd2a96dbd5b0b46
Mattia Gottardello <mattia.gottardello.96@gmail.com>
2020-04-30 12:52:23 +0200

    added fileExplorerController getBody()

--- nodeJS/src/controllers/fileExplorerController.js
+++ nodeJS/src/controllers/fileExplorerController.js
@@ -2,8 +2,21 @@
 // requires model and view
 let view = require('../views/fileExplorerView');
 let model = require('../models/fileExplorerModel');
-exports.getBody = (req,res) => {
+
+/**
+ * Effettua il rendering del body della pagina di visualizzazione dei file in base ai parametri contenuti nel json fileList. Se fileList
+ * contiene una lista vuota farà il rendereing di un avvertimento.
+ * @param {object} res - Rappresenta la risposta http
+ */
+exports.getBody = (res) => {
     //Variabile fileList di prova (da costruire prendendo i file da s3)
-    fileList = { files:[{fileKey: 'titoloProva',thumbnailUrl: 'urlThumbnail'},{fileKey: 'PartitaDiCalcio',thumbnailUrl: 'urlImmagine'}] };
-    view.print(req,res,fileList);
+    const fileKeys = fileExplorerModel.getUpdateFileList();
+    let fileList = Array();
+    for (key in fileKeys){
+        fileList.push({
+            fileKey: key,
+            thumbnailURL: fileExplorerModel.getThumbnailURL(key)
+        });
+    }
+    view.print(fileList,res);
 }
