a10c98e7422ea31edcca1e9bca57b7935e7161aa
Alessio Bettarello <bettaz.developer@gmail.com>
2020-04-30 15:53:31 +0200

    add snsWrapper and getProgression in loadingModel

--- nodeJS/src/models/loadingModel.js
+++ nodeJS/src/models/loadingModel.js
@@ -2,3 +2,11 @@
  * Loading Model module.
  * @module models/loadingModel
  */
+
+// Requires the AWS SNS wrapper
+const sns = require('../wrappers/snsWrapper');
+
+exports.getProgression = () => {
+    //TODO rimuovere la funzione e anche il model, inutile
+    return sns.read('progression');
+};
