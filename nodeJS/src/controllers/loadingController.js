2546d0326721d7a42ada107972f559bb50b656dc
Alessio Bettarello <bettaz.developer@gmail.com>
2020-04-30 15:23:37 +0200

    add loadingController - getUpdatedBar

--- nodeJS/src/controllers/loadingController.js
+++ nodeJS/src/controllers/loadingController.js
@@ -9,3 +9,13 @@ model= require('../models/loadingModel');
 exports.getBody = (req,res) => {
     view.print(req,res);
 }
+
+/**
+ * Funzione per ​recuperare il livello di progressione tramite il
+ * loadingModel e produrre una nuova barra di progressione XHTML aggiornata
+ * @param {object} res - Rappresenta un oggetto XHTML da ritornare
+ */
+exports.getUpdatedBar = (res) => {
+    const progression = model.getProgression();
+    view.generateProgressBar(res,progression);
+}
