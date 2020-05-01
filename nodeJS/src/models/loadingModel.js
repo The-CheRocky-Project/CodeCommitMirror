4a057e6ed69e8244db357f3e151f311b723bbab4
Alessio Bettarello <bettaz.developer@gmail.com>
2020-05-01 14:48:46 +0200

    removed model content

--- nodeJS/src/models/loadingModel.js
+++ nodeJS/src/models/loadingModel.js
@@ -1,12 +1,12 @@
-/**
- * Loading Model module.
- * @module models/loadingModel
- */
-
-// Requires the AWS SNS wrapper
-const sns = require('../wrappers/snsWrapper');
-
-exports.getProgression = () => {
-    //TODO rimuovere la funzione e anche il model, inutile
-    return sns.read('progression');
-};
+// /** TODO remove all also from the manual
+//  * Loading Model module.
+//  * @module models/loadingModel
+//  */
+//
+// // Requires the AWS SNS wrapper
+// const sns = require('../wrappers/snsWrapper');
+//
+// exports.getProgression = () => {
+//     //TODO rimuovere la funzione e anche il model, inutile
+//     return sns.read('progression');
+// };
