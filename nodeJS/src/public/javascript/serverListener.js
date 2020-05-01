ba18d019ccfd0c866b597f99a03ab43f641204e2
Nicola Panozzo <nicolapanoz@gmail.com>
2020-05-01 17:01:39 +0200

    comment part of code for testing

--- nodeJS/src/public/javascript/serverListener.js
+++ nodeJS/src/public/javascript/serverListener.js
@@ -80,43 +80,43 @@ $(document).ready(() => {
       updateFileList();
     });
 
-
-    $("#carica").click(() => {
-        alert("Caricamento da interfaccia non ancora disponibile, utilizzare AWS CLI");
-    });
-    $("#start").click(function(){
-        $.ajax({
-            url: "./toLoading"
-        }).done(function(data){
-            window.location.reload();
-        })
-    });
-    $("#progress-style").click(function(){
-        $.ajax({
-            url: "./toEdit"
-        }).done(function(data){
-            window.location.reload();
-        })
-    });
-    $("#backLink").click(function(){
-        $.ajax({
-            url: "./toFileExplorer"
-        }).done(function(data){
-            window.location.reload();
-        })
-    });
-    $('#confirm').click(() => {
-        alert("Non è ancora possibile eseguire il montaggio");
-    });
-    $('#inputGroupSelect01').change(() => {
-        alert("Non è ancora possibile cambiare la modalità video");
-    });
-    $('#tableReset').click(() => {
-        alert("Non è ancora possibile ripristinare i valori della tabella agli originali");
-    });
-    $('#addRow').click(() => {
-        alert("Non è ancora possibile aggiungere righe alla tabella dei riconoscimenti");
-    });
+// FORSE UTILE PER TESTARE
+    // $("#carica").click(() => {
+    //     alert("Caricamento da interfaccia non ancora disponibile, utilizzare AWS CLI");
+    // });
+    // $("#start").click(function(){
+    //     $.ajax({
+    //         url: "./toLoading"
+    //     }).done(function(data){
+    //         window.location.reload();
+    //     })
+    // });
+    // $("#progress-style").click(function(){
+    //     $.ajax({
+    //         url: "./toEdit"
+    //     }).done(function(data){
+    //         window.location.reload();
+    //     })
+    // });
+    // $("#backLink").click(function(){
+    //     $.ajax({
+    //         url: "./toFileExplorer"
+    //     }).done(function(data){
+    //         window.location.reload();
+    //     })
+    // });
+    // $('#confirm').click(() => {
+    //     alert("Non è ancora possibile eseguire il montaggio");
+    // });
+    // $('#inputGroupSelect01').change(() => {
+    //     alert("Non è ancora possibile cambiare la modalità video");
+    // });
+    // $('#tableReset').click(() => {
+    //     alert("Non è ancora possibile ripristinare i valori della tabella agli originali");
+    // });
+    // $('#addRow').click(() => {
+    //     alert("Non è ancora possibile aggiungere righe alla tabella dei riconoscimenti");
+    // });
 });
 
 function goIndex() {
