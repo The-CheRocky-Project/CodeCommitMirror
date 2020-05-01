b8f6ee7bb71939bf857915663d50abcc9713f6c1
Mattia Gottardello <mattia.gottardello.96@gmail.com>
2020-05-01 15:58:44 +0200

    added setPreviewMode() e setOriginalMode()

--- nodeJS/src/models/editModel.js
+++ nodeJS/src/models/editModel.js
@@ -4,3 +4,32 @@
  */
 const s3Wrap = require('../wrappers/s3Wrapper');
 const snsWrap = require('../wrappers/snsWrapper');
+
+
+// exports.getVideoEndpoint = () =>{
+//     return const videoURL = s3Wrap.getObjectUrl(fileKey);
+// };
+//
+// exports.getRecognizementList = () =>{
+//
+// };
+
+/**
+ * Effettua la comunicazione di impostare il video in modalità preview.
+ * @returns {boolean} true se la chiamata è stata effettuata con successo, false altrimenti.
+ */
+exports.setPreviewMode = () =>{
+    return snsWrap.message({
+        message: "setPreview"
+    })
+};
+
+/**
+ * Effettua la comunicazione di impostare il video in modalità originale.
+ * @returns {boolean} true se la chiamata è stata effettuata con successo, false altrimenti.
+ */
+exports.setOriginalMode = () =>{
+    return snsWrap.message({
+        message: "setOriginal"
+    })
+};
