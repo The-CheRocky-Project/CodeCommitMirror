2ee4e7dae98b689f552e92ee37015d6b85eed31c
Alessio Bettarello <bettaz.developer@gmail.com>
2020-05-02 13:03:29 +0200

    add snsWrap initialization

--- nodeJS/src/wrappers/snsWrapper.js
+++ nodeJS/src/wrappers/snsWrapper.js
@@ -2,3 +2,7 @@
  * snsWrapper module
  * @module wrappers/snsWrapper
  */
+
+//Richiede i moduli sdk necessari e ne crea un istanza
+const AWS = require('aws-sdk');
+const snsClient = new AWS.SNS();
