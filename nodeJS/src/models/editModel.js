4c96e80228f7f64319bcf37750e03fa65164d69b
Alessio Bettarello <bettaz.developer@gmail.com>
2020-05-02 12:13:54 +0200

    correct some requests

--- nodeJS/src/models/editModel.js
+++ nodeJS/src/models/editModel.js
@@ -17,13 +17,22 @@ const originalPrefixes = {
     false: "preview"
 };
 
+//Bucket e region di default
+const s3Defaults ={
+    bucket: "ahlconsolebucket",
+    region: "us-east-2"
+}
+
 
 /**
  * Effettua la comunicazione di impostare il video in modalità preview.
  * @returns {boolean} true se la chiamata è stata effettuata con successo, false altrimenti.
  */
 exports.getVideoEndpoint =  () =>{
-    return s3Wrap.getObjectUrl(originalPrefixes[actualVideoKey.original] + actualVideoKey.key);
+    return s3Wrap.getObjectUrl(
+        originalPrefixes[actualVideoKey.original] + actualVideoKey.key,
+        s3Defaults.bucket,
+        s3Defaults.region);
 };
 
 //
@@ -47,18 +56,20 @@ exports.setPreviewMode = async () =>{
  * Effettua la comunicazione di impostare il video in modalità originale.
  * @returns {boolean} true se la chiamata è stata effettuata con successo, false altrimenti.
  */
-exports.setOriginalMode = () =>{
-    return snsWrap.message({
+exports.setOriginalMode = async () =>{
+    if(await snsWrap.message({
         message: "setOriginal"
-    })
+    })){
+        actualVideoKey.original = true;
+    }
 };
 
 /**
  * Effettua il recupero del file contenente le labels tramite l's3Wrapper.
  * @returns {object} dizionario Json contenente le label impostate secondo le cartelle del modello.
  */
-exports.getmodelLabels = () =>{
-    return s3Wrap.getJsonFile("labels");
+exports.getmodelLabels = async () =>{
+    return await s3Wrap.getJsonFile(s3Defaults.bucket,"labels.json");
 };
 
 
@@ -66,8 +77,8 @@ exports.getmodelLabels = () =>{
  * Effettua la richiesta di aggiunta di una riga al sistema secondo i parametri specificati dal dizionario json params.
  * @param {number} modelIndex - Rappresenta il dizionario Kson contenente tutte le specifiche della riga da inserire
  */
-exports.addRow = (params) =>{
-    return snsWrap.message({
+exports.addRow = async (params) =>{
+    return await snsWrap.message({
         target: "addRow",
         start: params['start'],
         duration: param['duration'],
