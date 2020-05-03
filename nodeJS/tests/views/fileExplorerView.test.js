c4fbaeb07f9d31ee66789af86cf62183df22d13d
Francesco <Freda>
2020-05-03 12:07:29 +0200

    adjust fileExplorerView.test

--- nodeJS/tests/views/fileExplorerView.test.js
+++ nodeJS/tests/views/fileExplorerView.test.js
@@ -1,21 +1,18 @@
 const view = require('../../src/views/fileExplorerView');
 const assert = require('assert');
 
-let param = {};
-let mockRes = {
-    viewName: "",
-    viewData: {},
-    render: function(viewName, viewData) {
-        this.viewName = viewName;
-        this.viewData = viewData;
-    }
-};
-
-describe('fileExplorerView', () => {
-    describe('print()', () => {
-        it("should render the fileExplorerTemplate", () =>{
-           view.print(param,mockRes);
-           assert.equal(mockRes.viewName,"layouts/fileExplorerTemplate");
-        });
+describe('testFileExplorerView', () => {
+    it("testPrint", () =>{
+        let param = {};
+        let mockRes = {
+            viewName: "",
+            viewData: {},
+            render: function(viewName, viewData) {
+                this.viewName = viewName;
+                this.viewData = viewData;
+            }
+        };
+        view.print(param,mockRes);
+        assert.equal(mockRes.viewName,"layouts/fileExplorerTemplate");
     });
 });
