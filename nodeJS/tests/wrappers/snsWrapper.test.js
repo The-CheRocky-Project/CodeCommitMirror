cc30cd25bab2161728e28017876120f138f6e4ba
Francesco <Freda>
2020-05-04 11:37:19 +0200

    add a test case on snsWrapper

--- nodeJS/tests/wrappers/snsWrapper.test.js
+++ nodeJS/tests/wrappers/snsWrapper.test.js
@@ -8,15 +8,39 @@ describe('testSnsWrapper',() => {
       let topic = 'myTopic';
       let region = 'us-east-2';
       let userCode = 'myCode';
-      let arn = snsWrap.getTopicArn(topic, region, userCode);
     });
 
-    it("should get the correct ARN", () => {
+    it("deve restituire l'ARN correttamente calcolato di un topic", () => {
       let expectedARN="arn:aws:sns:"+this.region+":"+this.userCode+":"+this.topic;
       let arn = snsWrap.getTopicArn(this.topic, this.region, this.userCode);
       assert.strictEqual(arn, expectedARN);
     });
-
+  });
+  describe('#sendMessage()', ()=> {
+    before(() => {
+      var msg= {
+        Message: "message",
+        MessageAttributes: {
+            'data': {
+                DataType: "dataFormat",
+                BinaryValue: Buffer.from("data")
+            }
+        },
+        TopicArn: this.arn
+      };
+      AWS.mock('SNS', 'publish', (params, callback) => {
+        callback(undefined, 'success'); // Mocked response returns ‘success’ always
+      });
+    });
+    it("deve effettuare la pubblicazione di un messaggio con esito positivo", () => {
+      var expectedValue=true;
+      var result= snsWrap.publisher(this.msg);
+      assert.strictEqual(result,expectedValue);
+      
+    });
+    after(() => {
+      AWS.restore('SNS', 'publish');
+    });
     /*it("testEsitoPositivoRichiestaInvioMessaggio", () => {
       var expectedResult=true;
       //var topicPub = new snsWrap.TopicPublisher(this.topic, this.region, this.userCode);
