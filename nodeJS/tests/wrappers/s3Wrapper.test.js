f70fd781efbd2618a85a941079d2146c4200cc3d
Mirco <giardinamirco@gmail.com>
2020-05-04 20:09:18 +0200

    Added s3Wrapper.getJsonFile() unit test

--- nodeJS/tests/wrappers/s3Wrapper.test.js
+++ nodeJS/tests/wrappers/s3Wrapper.test.js
@@ -1,12 +1,8 @@
 const assert = require('assert');
 const rewire = require("rewire");
 let s3Wrap = rewire("../../src/wrappers/s3Wrapper");
-//let s3Wrap = require('../../src/wrappers/s3Wrapper');
-//const AWS = require('aws-sdk-mock');
-
 
 describe('testS3Wrapper', function() {
-
     describe('#getObjectUrl()', () => {
         it("should get the correct object url", () =>{
             let fileKey = "fileKeyDiProva";
@@ -36,9 +32,39 @@ describe('testS3Wrapper', function() {
                 }
             }
             s3Wrap.__set__("s3Client", s3ClientMock);
-            let objects = s3Wrap.listObjects(this.bucket,this.prefix);
+            let objects = s3Wrap.listObjects(bucket,prefix);
             objects.then(function (res) {
                 assert.deepStrictEqual(res,["happyface.mp4","test.mp4"]);
+            })
+                .catch(function (err) {
+                    assert.fail("Promise error: "+err);
+                })
+        });
+    });
+    describe('#getJsonFile()', () => {
+        it("should get the json content", () =>{
+            let bucket = "prova";
+            let fileKey = "prova.json";
+            let response = {
+                Body: '{"campo1":"prova","campo2":"ciao"}'
+            }
+            let s3ClientMock = {
+                getObject : function() {
+                    return {
+                        promise: function() {
+                            return response;
+                        }
+                    }
+                }
+            }
+            s3Wrap.__set__("s3Client", s3ClientMock);
+            let json = s3Wrap.getJsonFile(bucket,fileKey);
+            json.then(function (res) {
+                expectedRes = {
+                    campo1 : "prova",
+                    campo2 : "ciao"
+                }
+                assert.deepStrictEqual(res,expectedRes);
             })
                 .catch(function (err) {
                     assert.fail("Promise error: "+err);
