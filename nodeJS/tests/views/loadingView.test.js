99432ec21ea6ed2123d073dacd84b47ae40e660f
Mirco <giardinamirco@gmail.com>
2020-05-05 17:42:55 +0200

    Added beforeEach() instead of using local variables

--- nodeJS/tests/views/loadingView.test.js
+++ nodeJS/tests/views/loadingView.test.js
@@ -2,30 +2,25 @@ const view = require('../../src/views/loadingView');
 const assert = require('assert');
 
 describe('testLoadingView', () => {
+    let mockRes;
+    beforeEach(function() {
+        mockRes = {
+            viewName: "",
+            viewData: {},
+            render: function(viewName, viewData) {
+                this.viewName = viewName;
+                this.viewData = viewData;
+            }
+        };
+    });
     describe('#print()', () => {
         it("should render the loadingTemplate", () =>{
-            let mockRes = {
-                viewName: "",
-                viewData: {},
-                render: function(viewName, viewData) {
-                    this.viewName = viewName;
-                    this.viewData = viewData;
-                }
-            };
             view.print(mockRes);
             assert.strictEqual(mockRes.viewName,"layouts/loadingTemplate");
         });
     });
     describe('#generateProgressBar()', () => {
         it("should render the progress bar with the correct progression", () =>{
-            let mockRes = {
-                viewName: "",
-                viewData: {},
-                render: function(viewName, viewData) {
-                    this.viewName = viewName;
-                    this.viewData = viewData;
-                }
-            };
             let progress = 100;
             view.generateProgressBar(mockRes,progress);
             assert.strictEqual(mockRes.viewName,"partials/progressBar");
