55733d20491773b3a5f86f4cda760010641d30b0
Mirco <giardinamirco@gmail.com>
2020-05-05 17:17:41 +0200

    Added loadingView.generateProgressBar() unit test

--- nodeJS/tests/views/loadingView.test.js
+++ nodeJS/tests/views/loadingView.test.js
@@ -16,4 +16,20 @@ describe('testLoadingView', () => {
             assert.strictEqual(mockRes.viewName,"layouts/loadingTemplate");
         });
     });
+    describe('#generateProgressBar()', () => {
+        it("should render the progress bar with the correct progression", () =>{
+            let mockRes = {
+                viewName: "",
+                viewData: {},
+                render: function(viewName, viewData) {
+                    this.viewName = viewName;
+                    this.viewData = viewData;
+                }
+            };
+            let progress = 100;
+            view.generateProgressBar(mockRes,progress);
+            assert.strictEqual(mockRes.viewName,"partials/progressBar");
+            assert.strictEqual(mockRes.viewData.progression,100);
+        });
+    });
 });
