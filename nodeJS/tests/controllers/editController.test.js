2158acee1a9f935fb6ea6410e4ffec6c0600e7d6
Francesco <Freda>
2020-05-08 00:57:35 +0200

    add some editController tests

--- nodeJS/tests/controllers/editController.test.js
+++ nodeJS/tests/controllers/editController.test.js
@@ -20,7 +20,9 @@ describe('testEditController',() => {
       assert.equal(updateRowIsCalled,expected);
     });
   });
+
   describe('#confirmEditing()', () => {
+
     it("deve notificare l’accettazione del contenuto della tabella al sistema", () => {
       let sendConfirmationIsCalled = false;
       mock('../../src/models/editModel', { sendConfirmation: function() {
@@ -33,6 +35,7 @@ describe('testEditController',() => {
       assert.equal(sendConfirmationIsCalled,expected);
     });
   });
+
   describe('#updateLabelTable()', () => {
     it("deve aggiornare il rendering della tabella dei riconoscimenti.", () => {
       let getRecongnizementListIsCalled = false;
@@ -54,7 +57,9 @@ describe('testEditController',() => {
       assert.equal(result,expected);
     });
   });
+
   describe('#changeVideoMode()', () => {
+
     it("deve impostare la modalità di visualizzazione su original", () => {
       let setOriginalModeIsCalled = false;
       mock('../../src/models/editModel', { setOriginalMode: function() {
@@ -67,6 +72,7 @@ describe('testEditController',() => {
       var expected = true;
       assert.equal(setOriginalModeIsCalled,expected);
     });
+
     it("deve impostare la modalità di visualizzazione su preview", () => {
       let setPreviewModeIsCalled = false;
       mock('../../src/models/editModel', { setPreviewMode: function() {
@@ -78,6 +84,87 @@ describe('testEditController',() => {
       editController.changeVideoMode(toOriginal);
       var expected = true;
       assert.equal(setPreviewModeIsCalled,expected);
+    });
+  });
+  describe('#updateVideoFrame()', () => {
+
+    it("deve generare e renderizzare il nuovo frame video", () => {
+      let getVideoEndpointIsCalled = false;
+      let getVideoTypeIsCalled = false;
+      mock('../../src/models/editModel', { 
+        getVideoEndpoint: function() {
+          getVideoEndpointIsCalled = true;
+        },
+        getVideoType: function() {
+          getVideoTypeIsCalled = true;
+        }
+      });
+      let generateVideoFrameIsCalled = false;
+      mock('../../src/views/editView', { generateVideoFrame: function(videoEndpoint, isOriginal, res) {
+        generateVideoFrameIsCalled = true;
+      }});
+      let mMock = require('../../src/models/editModel');
+      let vMock = require('../../src/views/editView');
+      editController.__set__('model', mMock);
+      editController.__set__('view', vMock);
+      var res = 'resMock';
+      editController.updateVideoFrame(res);
+      var expected = true;
+      var result = getVideoEndpointIsCalled && generateVideoFrameIsCalled && getVideoTypeIsCalled;
+      assert.equal(result,expected);
+    });
+  });
+  describe('#changeRowCheckBox()', () => {
+
+    it("deve effettuare la richiesta di selezionare una riga di riconoscimento", () => {
+      let checkRowIsCalled = false;
+      mock('../../src/models/editModel', { 
+        checkRow: function(index) {
+          checkRowIsCalled = true;
+        },
+      });
+      let mMock = require('../../src/models/editModel');
+      editController.__set__('model', mMock);
+      var index = 'indexMock', checked = true;
+      editController.changeRowCheckBox(index, checked);
+      var expected = true;
+      var result = checkRowIsCalled;
+      assert.equal(result,expected);
+    });
+
+    it("deve effettuare la richiesta di selezionare una riga di riconoscimento", () => {
+      let uncheckRowIsCalled = false;
+      mock('../../src/models/editModel', { 
+        uncheckRow: function(index) {
+          uncheckRowIsCalled = true;
+        },
+      });
+      let mMock = require('../../src/models/editModel');
+      editController.__set__('model', mMock);
+      var index = 'indexMock', checked = false;
+      editController.changeRowCheckBox(index, checked);
+      var expected = true;
+      var result = uncheckRowIsCalled;
+      assert.equal(result,expected);
+    });
+  });
+  describe('#changeRowValue()', () => {
+
+    it("deve effettuare la richiesta al sistema di modificare una riga della tabella dei riconoscimenti", () => {
+      let updateRowIsCalled = false;
+      mock('../../src/models/editModel', { 
+        updateRow: function(index,start,duration,label) {
+          updateRowIsCalled = true;
+        },
+      });
+      let mMock = require('../../src/models/editModel');
+      editController.__set__('model', mMock);
+      var res = 'resMock';
+      let index = 'indexMock', start = 'startMock', duration = 'durationMock', label = 'labelMock';
+      editController.changeRowValue(index,start,duration,label);
+      var expected = true;
+      var result = updateRowIsCalled;
+      assert.equal(result,expected);
     });
   });
 });
