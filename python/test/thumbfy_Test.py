a2df13e50874cb59b05be6eeabafca0ec68cb241
Mirco <giardinamirco@gmail.com>
2020-05-08 18:44:50 +0200

    Added negative result test for thumbfy lambda

--- python/test/thumbfy_Test.py
+++ python/test/thumbfy_Test.py
@@ -2,6 +2,7 @@ from python.src.thumbfy import lambda_handler
 from python.src.layers import media_manager
 import json
 import os
+import pytest
 
 # Percorso assouluto per caricare il file event.json
 absolute_path = os.path.dirname(os.path.abspath(__file__))
@@ -19,13 +20,24 @@ with open(file_path, 'r') as f:
 context = "fake"
 
 
-def test_thumbfy(monkeypatch):
-    # Mock della funzione createThumbnail()
-    def create_thumbnail_mock(input_key, output_folder_key, queue):
-        assert input_key == 's3://ahlconsolebucket/origin/partita_di_calcio.mp4'
-        assert output_folder_key == 's3://ahlconsolebucket/origin/partita_di_calcio.mp4.jpg'
-        assert queue == 'console_thumbnail'
-        return 10  # Fake job id
+class TestThumbfy:
+    def test_positive_result(self, monkeypatch):
+        # Mock della funzione createThumbnail()
+        def create_thumbnail_mock(input_key, output_folder_key, queue):
+            assert input_key == 's3://ahlconsolebucket/origin/partita_di_calcio.mp4'
+            assert output_folder_key == 's3://ahlconsolebucket/origin/partita_di_calcio.mp4.jpg'
+            assert queue == 'console_thumbnail'
+            return 10  # Fake job id
 
-    monkeypatch.setattr(media_manager, "create_thumbnail", create_thumbnail_mock)
-    assert lambda_handler(event_json, context) == 10
+        monkeypatch.setattr(media_manager, "create_thumbnail", create_thumbnail_mock)
+        assert lambda_handler(event_json, context) == 10
+
+    def test_negative_result(self, monkeypatch):
+        # Mock della funzione createThumbnail()
+        def create_thumbnail_mock(input_key, output_folder_key, queue):
+            raise Exception('Some error')
+
+        monkeypatch.setattr(media_manager, "create_thumbnail", create_thumbnail_mock)
+        # Controllo che sia stata lanciata un'eccezione
+        with pytest.raises(Exception):
+            lambda_handler(event_json, context)
