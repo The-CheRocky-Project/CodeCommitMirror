e11570ee6be05058968b7a1a42b0a3da099cac99
Francesco <Freda>
2020-05-11 00:02:09 +0200

    fix small line of code

---
+++ python/test/streamer_test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+import pytest
+from python.src.layers import media_manager
+from python.src.thumbfy import lambda_handler
+
+# Percorso assouluto per caricare il file event.json
+absolute_path = os.path.dirname(os.path.abspath(__file__))
+file_path = absolute_path + '/../event/thumbfy_event.json'
+
+# Carico il file json con l'evento di test
+"""
+    Campi importanti dell'event.json allo scopo di test:
+    - Records[0].s3.bucket.name
+    - Records[0].object.key
+"""
+with open(file_path, 'r') as f:
+    event_json = json.load(f)
+
+context = "fake"
