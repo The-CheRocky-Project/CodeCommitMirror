24c2394f6ac2a5af6894c336877e1ca3707dd627
Alessio Bettarello <bettaz.developer@gmail.com>
2020-05-28 18:51:29 +0200

    add an sns-validator

--- nodeJS/src/router.js
+++ nodeJS/src/router.js
@@ -257,44 +257,61 @@ ahl.post('/confirmEdit', (req,res) => {
 ahl.post('/notifyProgressionUpdate', async (req,res) => {
   // TODO sistemare la funzione e testarla
     console.log("Notify start");
-    if(req.body.Type == "SubscriptionConfirmation"){
-        console.log("Confirmation Script");
-        const AWS = require('aws-sdk');
-        const SNS = new AWS.SNS();
-        console.log("ReqBody", req.body);
-        let params = {
-            Token: req.body.Token,
-            TopicArn: req.body.TopicArn,
-            AuthenticateOnUnsubscribe: "false"
+    const https = require('https'),
+        MessageValidator = require('sns-validator'),
+        validator = new MessageValidator();
+    validator.validate(req.body, function (err, message) {
+        console.log('validator Start');
+        if (err) {
+            console.error(err);
+            return;
         }
-        await SNS.confirmSubscription(params).promise()
-            .then(data => console.log(data)
-                .catch(err => console.log(err, err.message))
-            );
-        console.log("Confirmation Script terminated");
-    }
-    else {
-        if(req.body.Type == "Notification") {
-            console.log("Notification Script");
-            backport.emit('progress', req.body.progress);
-            if (progression >= 100){
-                activePage = pages.edit;
-                backport.emit('refresh','');
-            }
-            console.log("Notification Script terminated");
+
+        if (message['Type'] === 'SubscriptionConfirmation') {
+            console.log("Subscription Start");
+            https.get(message['SubscribeURL'], function (res) {
+                console.log("You have confirmed your endpoint subscription");
+            });
+            console.log("Subscription End");
         }
+            // if(req.body.Type == "SubscriptionConfirmation"){
+            //     console.log("Confirmation Script");
+            //     const AWS = require('aws-sdk');
+            //     const SNS = new AWS.SNS();
+            //     console.log("ReqBody", req.body);
+            //     let params = {
+            //         Token: req.body.Token,
+            //         TopicArn: req.body.TopicArn,
+            //         AuthenticateOnUnsubscribe: "false"
+            //     }
+            //     let promise = SNS.confirmSubscription(params).promise();
+            //     promise.then(data => console.log(data));
+            //     promise.catch(err => console.log(err, err.message));
+            //     console.log("Confirmation Script terminated");
+        // }
         else {
-            /* TODO remove the following 5 lines when SNS Notification test is complete and change test content
-             changing the behaviour: unprocessable message */
-            console.log("Dummy Script");
-            backport.emit('progress',req.body['progress']);
-            if (progression >= 100){
-                activePage = pages.edit;
-                backport.emit('refresh','');
+            if (req.body.Type == "Notification") {
+                console.log("Notification Script");
+                backport.emit('progress', req.body.progress);
+                if (progression >= 100) {
+                    activePage = pages.edit;
+                    backport.emit('refresh', '');
+                }
+                console.log("Notification Script terminated");
+            } else {
+                /* TODO remove the following 5 lines when SNS Notification test is complete and change test content
+                 changing the behaviour: unprocessable message */
+                console.log("Dummy Script");
+                backport.emit('progress', req.body['progress']);
+                if (progression >= 100) {
+                    activePage = pages.edit;
+                    backport.emit('refresh', '');
+                }
+                console.log("Dummy Script terminated");
             }
-            console.log("Dummy Script terminated");
         }
-    }
+        console.log("Validator End");
+    });
     res.send('');
     console.log("notify End");
 });
