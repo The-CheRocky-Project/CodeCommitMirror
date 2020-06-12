1d68add3a00e781ed355c31a3cd214e95cc9bba6
Francesco <fifreda@hotmail.it>
2020-06-12 22:21:48 +0200

    update framer unit tests

--- python/test/framer_test.py
+++ python/test/framer_test.py
@@ -1,64 +1,67 @@
 # -*- coding: utf-8 -*-
 """
-Test module for json_splitter lambda
+Test module for framer lambda
 """
 import json
 import os
 import unittest
+import boto3
 from unittest.mock import patch
 from aws_lambda_context import LambdaContext
 from src.framer import lambda_handler
 
-# Percorso assouluto per caricare il file event.json
 absolute_path = os.path.dirname(os.path.abspath(__file__))
 file_path_true = absolute_path + '/../event/framer_event_start_process.json'
 file_path_false = absolute_path + '/../event/framer_event_not_start.json'
+file_path_exc = absolute_path + '/../event/framer_exception_event.json'
 
-# Carico il file json con l'evento di test
-"""
-    Campi importanti dell'event.json allo scopo di test:
-    - event["Records"][0]["Sns"]["Message"]
-    - event["Records"][0]["Sns"]["MessageAttributes"]["bucket"]["Value"]
-    - event["Records"][0]["Sns"]["MessageAttributes"]["key"]["Value"]
-"""
 with open(file_path_true, 'r') as f:
     event_json_true = json.load(f)
 with open(file_path_false, 'r') as f:
     event_json_false = json.load(f)
+with open(file_path_exc, 'r') as f:
+    event_json_exc = json.load(f)
 
 CONTEXT = LambdaContext()
 CONTEXT.function_name = 'framer'
 
 
 class TestFramer(unittest.TestCase):
-    """
-    Classe di test per il lambda_handler di framer
-    """
-    # TODO edit tests to fit with 0 notification (mock sns call
-    # because are creating a token error( codecommit couldnt call apis)
-    # def test_job_start_correctly(self):
-    #     with patch('boto3.client') as mock:
-    #         media_conv = mock.return_value
-    #         media_conv.create_job.return_value = {
-    #             'Job': {
-    #                 'Id': 'string'
-    #             },
-    #         }
-    #         expected = "string"
-    #         result = lambda_handler(event_json_true, CONTEXT)
-    #         self.assertEqual(expected, result)
-    #
-    # def test_job_doesnt_start_correctly(self):
-    #     expected = False
-    #     result = lambda_handler(event_json_false, CONTEXT)
-    #     self.assertEqual(expected, result)
-    #
-    # def test_job_exception(self):
-    #     with patch('boto3.client') as mock:
-    #         # media_conv = mock.return_value
-    #         # media_conv.create_job.raiseError.side_effect = Exception('error')
-    #         self.assertRaises(
-    #             Exception,
-    #             lambda_handler,
-    #             file_path_true,
-    #             CONTEXT)
+    def test_job_start_correctly(self):
+        with patch('boto3.client') as mock:
+            service = mock.return_value
+            service.create_job.return_value = {
+                'Job': {
+                    'Id': 'string'
+                },
+            }
+            service.publish.return_value = {
+                'MessageId': 'msgId'
+            }
+            expected = "string"
+            result = lambda_handler(event_json_true, CONTEXT)
+            self.assertEqual(expected, result)
+    
+    def test_job_doesnt_start_correctly(self):
+        with patch('boto3.client') as mock:
+            service = mock.return_value
+            service.create_job.return_value = {
+                'Job': {
+                    'Id': 'string'
+                },
+            }
+            expected = False
+            result = lambda_handler(event_json_false, CONTEXT)
+            self.assertEqual(expected, result)
+    
+    def test_event_malformed_exception(self):
+        with patch('boto3.client') as mock:
+            service = mock.return_value
+            service.create_job.return_value = {
+                'Job': {
+                    'Id': 'string'
+                },
+            }
+            self.assertRaises(Exception, lambda_handler, event_json_exc, CONTEXT)
+
+    
