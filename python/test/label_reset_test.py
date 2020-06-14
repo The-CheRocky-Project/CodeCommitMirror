171e82251517bd750cf213d673ccd6cda8765a49
Alessio Bettarello <bettaz.developer@gmail.com>
2020-06-14 23:42:05 +0200

    add additional aux function for prepare resources

--- python/test/label_reset_test.py
+++ python/test/label_reset_test.py
@@ -32,6 +32,24 @@ CONTEXT = LambdaContext()
 CONTEXT.function_name = 'label_reset'
 
 
+def create_resources():
+    s3 = boto3.resource('s3')
+    s3.create_bucket(Bucket='ahlconsolebucket')
+    old_resume = s3.Object('ahlconsolebucket', 'tmp/modified-resume.json')
+    arr = [{"frame_key": "frames/match.0000001.jpg", "accuracy": "0.916056", "label": "6", "start": "3250",
+            "tfs": "6250", "type": "machine", "show": "true"},
+           {"frame_key": "frames/match.0000002.jpg", "accuracy": "0.969301", "label": "6", "start": "4750",
+            "tfs": "8750", "type": "machine", "show": "true"}]
+    old_resume.put(Body=json.dumps(arr))
+    s3.create_bucket(Bucket='ahlconsolebucket')
+    new_resume = s3.Object('ahlconsolebucket', 'tmp/resume.json')
+    arr = [{"frame_key": "frames/match.0000001.jpg", "accuracy": "0.916056", "label": "6", "start": "3250",
+            "tfs": "6", "type": "machine", "show": "true"},
+           {"frame_key": "frames/match.0000002.jpg", "accuracy": "0.969301", "label": "6", "start": "4750",
+            "tfs": "87", "type": "machine", "show": "true"}]
+    new_resume.put(Body=json.dumps(arr))
+    return old_resume, new_resume
+
 class TestLabelReset(unittest.TestCase):
     """
     Classe di test per il lambda_handler di label_reset
@@ -40,21 +58,7 @@ class TestLabelReset(unittest.TestCase):
     @mock_s3
     def test_reset_rows(self):
         with patch('boto3.client') as mock:
-            s3 = boto3.resource('s3')
-            s3.create_bucket(Bucket='ahlconsolebucket')
-            old_resume = s3.Object('ahlconsolebucket', 'tmp/modified-resume.json')
-            arr = [{"frame_key": "frames/match.0000001.jpg", "accuracy": "0.916056", "label": "6", "start": "3250",
-                    "tfs": "6250", "type": "machine", "show": "true"},
-                   {"frame_key": "frames/match.0000002.jpg", "accuracy": "0.969301", "label": "6", "start": "4750",
-                    "tfs": "8750", "type": "machine", "show": "true"}]
-            old_resume.put(Body=json.dumps(arr))
-            s3.create_bucket(Bucket='ahlconsolebucket')
-            new_resume = s3.Object('ahlconsolebucket', 'tmp/resume.json')
-            arr = [{"frame_key": "frames/match.0000001.jpg", "accuracy": "0.916056", "label": "6", "start": "3250",
-                    "tfs": "6", "type": "machine", "show": "true"},
-                   {"frame_key": "frames/match.0000002.jpg", "accuracy": "0.969301", "label": "6", "start": "4750",
-                    "tfs": "87", "type": "machine", "show": "true"}]
-            new_resume.put(Body=json.dumps(arr))
+            old_resume, new_resume = create_resources()
             sns_mock = mock.return_value
             sns_mock.publish.return_value = {
                 'MessageId': 'string'
@@ -64,22 +68,8 @@ class TestLabelReset(unittest.TestCase):
             self.assertEqual((old_resume.get())['Body'].read(), (new_resume.get())['Body'].read())
 
     @mock_s3
-    def test_not_edit_row(self):
-        s3 = boto3.resource('s3')
-        s3.create_bucket(Bucket='ahlconsolebucket')
-        old_resume = s3.Object('ahlconsolebucket', 'tmp/modified-resume.json')
-        arr = [{"frame_key": "frames/match.0000001.jpg", "accuracy": "0.916056", "label": "6", "start": "3250",
-                "tfs": "6250", "type": "machine", "show": "true"},
-               {"frame_key": "frames/match.0000002.jpg", "accuracy": "0.969301", "label": "6", "start": "4750",
-                "tfs": "8750", "type": "machine", "show": "true"}]
-        old_resume.put(Body=json.dumps(arr))
-        s3.create_bucket(Bucket='ahlconsolebucket')
-        new_resume = s3.Object('ahlconsolebucket', 'tmp/resume.json')
-        arr = [{"frame_key": "frames/match.0000001.jpg", "accuracy": "0.916056", "label": "6", "start": "3250",
-                "tfs": "6", "type": "machine", "show": "true"},
-               {"frame_key": "frames/match.0000002.jpg", "accuracy": "0.969301", "label": "6", "start": "4750",
-                "tfs": "87", "type": "machine", "show": "true"}]
-        new_resume.put(Body=json.dumps(arr))
+    def test_not_reset_row(self):
+        old_resume, new_resume = create_resources()
         result = lambda_handler(event_json2, CONTEXT)
         self.assertFalse(result)
         self.assertNotEqual((old_resume.get())['Body'].read(), (new_resume.get())['Body'].read())
