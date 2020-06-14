af4e1bd22dadbbec9decd8fa28f0b1231601d7bb
Mirco <giardinamirco@gmail.com>
2020-06-14 09:44:57 +0200

    Fixed check_n_prioritize unit test

--- python/test/check_n_prioritize_test.py
+++ python/test/check_n_prioritize_test.py
@@ -33,22 +33,27 @@ class TestLabelAdder(unittest.TestCase):
             s3.create_bucket(Bucket='ahlconsolebucket')
             resume = s3.Object('ahlconsolebucket', 'frames.json')
             arr = [{"frame_key": "frames/match.0000001.jpg", "accuracy": "0.916056", "label": "6", "start": "3250",
-                    "tfs": "6250", "type": "machine", "show": "true"},
+                    "tfs": "290000", "type": "machine", "show": "true"},
                    {"frame_key": "frames/match.0000002.jpg", "accuracy": "0.969301", "label": "2", "start": "4750",
+                    "tfs": "2322200", "type": "machine", "show": "true"},
+                   {"frame_key": "frames/match.0000015.jpg", "accuracy": "0.969301", "label": "6", "start": "4750",
                     "tfs": "8750", "type": "machine", "show": "true"}]
             resume.put(Body=json.dumps(arr))
             result = lambda_handler(event_json, CONTEXT)
-            expectedRes ={
+            expectedRes = {
                 'key': 'tmp/resume.json'
             }
             self.assertEqual(result, expectedRes)
             resume_json = s3.Object('ahlconsolebucket', result["key"])
             resume_res = resume_json.get()
             all_frames = json.loads(resume_res['Body'].read().decode('utf-8'))
-            expectedFrames = [{"frame_key": "frames/match.0000001.jpg", "accuracy": "0.916056", "label": "6", "start": "3250",
-                         "tfs": "6250", "type": "machine", "show": "true"},
-                        {"frame_key": "frames/match.0000002.jpg", "accuracy": "0.969301", "label": "2", "start": "4750",
-                         "tfs": "8750", "type": "machine", "show": "true"}]
+            expectedFrames = [
+                {'frame_key': 'frames/match.0000001.jpg', 'accuracy': '0.916056', 'label': '6', 'start': '3250',
+                 'tfs': '290000', 'type': 'machine', 'show': 'true'},
+                {'frame_key': 'frames/match.0000002.jpg', 'accuracy': '0.969301', 'label':
+                    '2', 'start': '4750', 'tfs': '2322200', 'type': 'machine', 'show': 'false'},
+                {'frame_key': 'frames/match.0000015.jpg', 'accuracy': '0.969301', 'label': '6', 'start': '4750',
+                 'tfs': '8750', 'type': 'machine', 'show': 'true'}]
             self.assertEqual(all_frames, expectedFrames)
 
     def test_check_n_prioritize_false(self):
