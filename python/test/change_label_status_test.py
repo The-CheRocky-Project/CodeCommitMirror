0eea96d19490b0097a6a7e352908ecdb6d200ae4
Mirco <giardinamirco@gmail.com>
2020-06-14 11:00:50 +0200

    Completed change_label_status unit test

--- python/test/change_label_status_test.py
+++ python/test/change_label_status_test.py
@@ -14,9 +14,17 @@ from src.change_label_status import lambda_handler
 # Percorso assouluto per caricare il file event.json
 absolute_path = os.path.dirname(os.path.abspath(__file__))
 file_path1 = absolute_path + '/../event/change_label_status_checkRow_event.json'
+file_path2 = absolute_path + '/../event/change_label_status_uncheckRow_event.json'
+file_path3 = absolute_path + '/../event/change_label_status_false_event.json'
 
 with open(file_path1, 'r') as f:
     event_json1 = json.load(f)
+
+with open(file_path2, 'r') as f:
+    event_json2 = json.load(f)
+
+with open(file_path3, 'r') as f:
+    event_json3 = json.load(f)
 
 CONTEXT = LambdaContext()
 CONTEXT.function_name = 'change_label_status'
@@ -28,7 +36,7 @@ class TestChangeLabelStatus(unittest.TestCase):
     """
 
     @mock_s3
-    def test_change_label_status_true(self):
+    def test_change_label_status_checkrow(self):
         with patch('boto3.client') as mock:
             s3 = boto3.resource('s3')
             s3.create_bucket(Bucket='ahlconsolebucket')
@@ -57,3 +65,54 @@ class TestChangeLabelStatus(unittest.TestCase):
                 {'frame_key': 'frames/match.0000015.jpg', 'accuracy': '0.969301', 'label': '6', 'start': '4750',
                  'tfs': '8750', 'type': 'machine', 'show': 'false'}]
             self.assertEqual(all_frames, expectedFrames)
+
+    @mock_s3
+    def test_change_label_status_uncheckrow(self):
+        with patch('boto3.client') as mock:
+            s3 = boto3.resource('s3')
+            s3.create_bucket(Bucket='ahlconsolebucket')
+            resume = s3.Object('ahlconsolebucket', 'tmp/modified-resume.json')
+            resumeJson = [
+                {"frame_key": "frames/match.0000001.jpg", "accuracy": "0.916056", "label": "6", "start": "3250",
+                 "tfs": "290000", "type": "machine", "show": "true"},
+                {"frame_key": "frames/match.0000002.jpg", "accuracy": "0.969301", "label": "2", "start": "4750",
+                 "tfs": "2322200", "type": "machine", "show": "true"},
+                {"frame_key": "frames/match.0000015.jpg", "accuracy": "0.969301", "label": "6", "start": "4750",
+                 "tfs": "8750", "type": "machine", "show": "true"}]
+            resume.put(Body=json.dumps(resumeJson))
+            snsMock = mock.return_value
+            snsMock.publish.return_value = {
+                'MessageId': 'string'
+            }
+            result = lambda_handler(event_json2, CONTEXT)
+            self.assertTrue(result)
+            resume_res = resume.get()
+            all_frames = json.loads(resume_res['Body'].read().decode('utf-8'))
+            expectedFrames = [
+                {'frame_key': 'frames/match.0000001.jpg', 'accuracy': '0.916056', 'label': '6', 'start': '3250',
+                 'tfs': '290000', 'type': 'machine', 'show': 'true'},
+                {'frame_key': 'frames/match.0000002.jpg', 'accuracy': '0.969301', 'label':
+                    '2', 'start': '4750', 'tfs': '2322200', 'type': 'machine', 'show': 'false'},
+                {'frame_key': 'frames/match.0000015.jpg', 'accuracy': '0.969301', 'label': '6', 'start': '4750',
+                 'tfs': '8750', 'type': 'machine', 'show': 'true'}]
+            self.assertEqual(all_frames, expectedFrames)
+
+    @mock_s3
+    def test_change_label_status_false(self):
+        s3 = boto3.resource('s3')
+        s3.create_bucket(Bucket='ahlconsolebucket')
+        resume = s3.Object('ahlconsolebucket', 'tmp/modified-resume.json')
+        resumeJson = [
+            {"frame_key": "frames/match.0000001.jpg", "accuracy": "0.916056", "label": "6", "start": "3250",
+             "tfs": "290000", "type": "machine", "show": "true"},
+            {"frame_key": "frames/match.0000002.jpg", "accuracy": "0.969301", "label": "2", "start": "4750",
+             "tfs": "2322200", "type": "machine", "show": "true"},
+            {"frame_key": "frames/match.0000015.jpg", "accuracy": "0.969301", "label": "6", "start": "4750",
+             "tfs": "8750", "type": "machine", "show": "true"}]
+        resume.put(Body=json.dumps(resumeJson))
+        result = lambda_handler(event_json3, CONTEXT)
+        self.assertFalse(result)
+
+    @mock_s3
+    def test_change_label_status_exception(self):
+        self.assertRaises(Exception, lambda_handler, event_json1, CONTEXT)
