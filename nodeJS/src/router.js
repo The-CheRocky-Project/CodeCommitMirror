f848f9ada9d8c4c40b913de7c2570a8fd97c5ff8
Alessio Bettarello <bettaz.developer@gmail.com>
2020-06-16 23:54:54 +0200

    test key fetch direct from body of the request

--- nodeJS/src/router.js
+++ nodeJS/src/router.js
@@ -409,7 +409,7 @@ ahl.post('/notifyNewVideoEndpoint', (req,res) => {
         if(req.body.Type == "Notification" && req.body.Message == 'videoEndpoint'){
             console.log("received message on notifyNewVideoEndpoint:" + req.body.Message + req.body.MessageAttributes);
             res.sendStatus(200);
-            const videoKey = req.body.MessageAttributes.key.StringValue;
+            const videoKey = req.body.key.StringValue;
             console.log("Received videoKey " + videoKey);
             editController.changeVideo(videoKey);
             if(activePage !== pages.edit){
